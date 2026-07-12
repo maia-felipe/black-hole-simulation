10e88c964b9ae43109054e26366183da9a25682b
maiafelipe0911 <fe.maia@klauvi.com>
2026-07-11 23:34:47 -0300

    got sleepy and gave up

--- src/main.cpp
+++ src/main.cpp
@@ -2,19 +2,5 @@
 #include "vec3.h"
 
 int main(){
-    Vec3 a(1.0, 2.0, 3.0);
-    Vec3 b(4.0, 5.0, 6.0);
-
-    Vec3 c = a+b;
-
-    std::cout << c.x << " " << c.y << " " << c.z <<"\n";
-    std::cout << dot(a,b) << "\n";
-    std::cout << a.length() << "\n";
-
-    a = -a;
-
-    c = c+a;
-
-    std::cout << c.x << " " << c.y << " " << c.z <<"\n";
-
+    
 }
